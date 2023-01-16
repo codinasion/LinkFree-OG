36badcf78fa963795d4a15d3d55be4d3a12299e7
Harsh Raj <harshraj8843@gmail.com>
2023-01-17 00:05:26 +0530

    add: LinkFree OG API

---
+++ api/README.md
@@ -0,0 +1,13 @@
+# LinkFree OG API
+
+This API is used to show LinkFree OG images.
+
+## Paths
+
+### /
+
+This path shows the default og image for LinkFree.
+
+### /{username}
+
+This path shows the og image for the user with the given username. If the usename is invalid, it will empty image.
---
+++ api/pages/api/[username].ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import fetch from "node-fetch"
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Buffer>
+) {
+    // Get username from request
+    const { username } = req.query;
+
+    // fetch image from github as buffer
+    const buffer = await fetch(
+        `https://raw.githubusercontent.com/codinasion/LinkFree-OG/og/profile/${username}.png`,
+        {
+            method: "GET",
+            headers: {
+                Authorization: `token ${process.env.GITHUB_TOKEN}`,
+            },
+        }
+    ).then((res) => res.arrayBuffer());
+
+    // Send image as buffer
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate=60");
+    res.status(200).send(Buffer.from(buffer));
+}
